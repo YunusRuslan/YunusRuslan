8556e9056c276ea00d4853b415d3708290eb0db9
Ruslan Yunus <obechayuobechat@gmail.com>
2024-11-16 21:38:17 +0500

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,27 @@
+name = "Ruslan"
+print("Name:", name)
+age = 40
+print("Age:", age)
+age += 1
+print("New age:", age)
+is_student = True
+print("Is_student:", is_student)
+
+# переменные.
+tasks_completed = 12
+hours_spent = 1.5
+course_name = 'Python'
+average_time_per_task = hours_spent / tasks_completed
+print("Курс:", course_name, ", всего задач:", tasks_completed, ", затрачено часов:", hours_spent, ", среднее время выполнения:", round(average_time_per_task, 3), "часа.")
+# Строки и Индексация строк
+name = 'Топинамбур'
+print(name[0])
+print(name[-1])
+print(name[5:])
+print(name[::-1])
+print(name[1::2])
+print('Топинамбур'[0])
+print('Топинамбур'[-1])
+print('Топинамбур'[5:])
+print('Топинамбур'[::-1])
+print('Топинамбур'[1::2])
